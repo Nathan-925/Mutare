0321b748a0edc7cb7d688178efeba32e97e7af24
Nathan <nathaniel.dunlap925@gmail.com>
2023-04-21 21:17:15 -0400

    Maybe closer

--- PerlinNoise.h
+++ PerlinNoise.h
@@ -28,19 +28,20 @@ namespace mut{
 			va_list list;
 			va_start(list, size);
 			this->size[0] = size;
-			int length = size;
+			int length = this->size[0];
 			for(int i = 1; i < dimensions; i++){
 				this->size[i] = va_arg(list, int);
 				std::cout << this->size[i] << std::endl;
 				length *= this->size[i];
 			}
 			va_end(list);
-			vectors = new double*[length];
+
 			std::cout << length << " " << dimensions << std::endl;
+			vectors = new double*[length];
 
 			std::default_random_engine rand(seed);
 			std::normal_distribution<double> distribution;
-			for(int i = 0; i < dimensions; i++){
+			for(int i = 0; i < length; i++){
 				vectors[i] = new double[dimensions];
 				double mag = 0;
 				for(int j = 0; j < dimensions; j++){
@@ -65,38 +66,45 @@ namespace mut{
 
 		double get(double position...){
 			double index[dimensions];
-			double fraction[dimensions];
 			va_list list;
 			va_start(list, position);
-			fraction[0] = modf(position*size[0], &index[0]);
-			for(int i = 1; i < dimensions; i++)
-				fraction[i] = modf(va_arg(list, double)*size[i], &index[i]);
+			index[0] = position;
+			std::cout << "d " << position << std::endl;
+			for(int i = 1; i < dimensions; i++){
+				double d = va_arg(list, double);
+				std::cout << "d " << d << std::endl;
+				index[i] = d;
+			}
 			va_end(list);
 			for(int i = 0; i < dimensions; i++)
-				std::cout << index[i] << " ";
+				std::cout << index[i] << "/" << index[i]/size[i] << " ";
 			std::cout << std::endl;
 
 			std::queue<double> queue;
 			for(int i = 0; i < pow(2, dimensions); i++){
 				int pos = 0;
+				std::cout  << std::endl << "p ";
 				for(int j = 0, offset = 1; j < dimensions; offset *= size[j], j++){
-					std::cout << ((i>>j)&1 ? 1+index[j] : index[j]) << " ";
-					pos += offset*((i>>j)&1 ? 1+index[j] : index[j]);
+					std::cout << ((i>>j)&1 ? std::ceil(index[j]) : std::floor(index[j])) << "/" << offset << " ";
+					pos += offset*((i>>j)&1 ? std::ceil(index[j]) : std::floor(index[j]));
+					std::cout << pos << " ";
 				}
-				std::cout << std::endl << std::endl;
+				std::cout << std::endl;
 
 				double dot = 0;
+				double temp;
 				for(int j = 0; j < dimensions; j++){
-					std::cout << pos << std::endl;
-					dot += vectors[pos][j]+((i>>j)&1 ? 1-fraction[j] : fraction[j]);
+					std::cout << pos << " " << j << std::endl;
+					dot += vectors[pos][j]+std::modf((i>>j)&1 ? 1-index[j] : index[j], &temp);
 				}
 				queue.push(dot);
 			}
 
+			double temp;
 			while(queue.size() > 1){
 				double d = queue.front();
 				queue.pop();
-				queue.push(d + smoothstep(fraction[(int)ceil(log2(queue.size()))])*(queue.front()-d));
+				queue.push(d + smoothstep(std::modf(index[(int)ceil(log2(queue.size()))], &temp))*(queue.front()-d));
 				queue.pop();
 			}
 
